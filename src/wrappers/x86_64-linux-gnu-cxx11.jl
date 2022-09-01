cddecf530f796af68f8057933f3b52b0692a6d93
jlbuild <juliabuildbot@gmail.com>
2022-09-01 22:05:14 +0000

    hsa_rocr_jll build 4.2.0+2

--- src/wrappers/x86_64-linux-gnu-cxx11.jl
+++ src/wrappers/x86_64-linux-gnu-cxx11.jl
@@ -2,13 +2,15 @@
 export libhsa_runtime64
 
 using hsakmt_roct_jll
+using ROCmDeviceLibs_jll
 using NUMA_jll
+using XML2_jll
 using Zlib_jll
 using Elfutils_jll
 JLLWrappers.@generate_wrapper_header("hsa_rocr")
 JLLWrappers.@declare_library_product(libhsa_runtime64, "libhsa-runtime64.so.1")
 function __init__()
-    JLLWrappers.@generate_init_header(hsakmt_roct_jll, NUMA_jll, Zlib_jll, Elfutils_jll)
+    JLLWrappers.@generate_init_header(hsakmt_roct_jll, ROCmDeviceLibs_jll, NUMA_jll, XML2_jll, Zlib_jll, Elfutils_jll)
     JLLWrappers.@init_library_product(
         libhsa_runtime64,
         "lib/libhsa-runtime64.so",
